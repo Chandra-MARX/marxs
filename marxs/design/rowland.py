6e0355648df29401a99a5b58ea91a1f3b8088ac1
hamogu <hgunther@mit.edu>
2015-06-12 14:49:48 -0400

    Add tests for GratingArrayStructure

--- marxs/design/rowland.py
+++ marxs/design/rowland.py
@@ -21,7 +21,9 @@ class RowlandTorus(object):
         return (x**2. + y**2. + z**2. + self.R**2. - self.r**2.)**2. - 4. * self.R**2. * (x**2. + y**2.)
     def normal(self, x, y, z):
         '''Return the gradient vector field'''
-        if not np.allclose(self.quartic(x, y, z), 0.):
+        # Makes problems fir r,R  >> 1. Even marginal differences lead to large
+        # numbers on the quartic because of R**4 -> normalize
+        if not np.allclose(self.quartic(x, y, z) / self.R**4., 0.):
             raise ValueError('Gradient vector field is only defined for points on torus surface.')
         factor = 4. * (x**2. + y**2. + z**2. + self.R**2. - self.r**2)
         dFdx = factor * x - 8. * self.R**2 * x
@@ -33,6 +35,14 @@ class GratingArrayStructure(object):
     '''
     Parameters
     ----------
+    rowland : RowlandTorus
+    d_facet : float
+        Size of the edge of a facet, which is assumed to be flat and square.
+        (``d_facet`` can be larger than the actual size of the silicon membrane to
+        accommodate a minimum thickness of the surrounding frame.)
+    x_range: list of 2 floats
+        Minimum and maximum of the x coordinate that is searched for an intersection
+        with the torus.
     radius : list of 2 floats float
         Inner and outer radius of the GAS as measured in the yz-plane from the
         origin.
@@ -42,13 +52,9 @@ class GratingArrayStructure(object):
         ``phi2`` in the usual mathematical way (counterclockwise).
         Angles are given in radian. Note that ``phi[1] < phi[0]`` is possible if
         the segment crosses the y axis.
-    d_facet : float
-        Size of the edge of a facet, which is assumed to be flat and square.
-        (``d_facet`` can be larger than the actual size of the silicon membrane to
-        accommodate a minimum thickness of the surrounding frame.)
-    z_min, z_max: TBD
     '''
-    def __init__(self, d_facet, phi, radius, z_min, z_max):
+    def __init__(self, rowland, d_facet, x_range, radius, phi=[0., 2*np.pi]):
+        self.rowland = rowland
         if not (radius[1] > radius[0]):
             raise ValueError('Outer radius must be larger than inner radius.')
         if np.min(radius) < 0:
@@ -61,10 +67,9 @@ class GratingArrayStructure(object):
 
         self.d_facet = d_facet
 
-        self.z_min = z_min
-        self.z_max = z_max
+        self.x_range = x_range
 
-        self.place_facets()
+        self.facets = self.place_facets()
 
     def max_facets_on_arc(self, radius):
         '''Calculate maximal; number of facets that can be placed at a certain radius.'''
@@ -93,16 +98,17 @@ class GratingArrayStructure(object):
         facet_angle = self.d_facet / (2. * np.pi * radius)
         # thickness of space between facets, distributed equally
         d_between = (anglediff - n * facet_angle) / (n + 1)
-        return d_between + 0.5 * facet_angle + np.arange(n) * (d_between + facet_angle)
+        centerangles = d_between + 0.5 * facet_angle + np.arange(n) * (d_between + facet_angle)
+        return (self.phi[0] + centerangles) % (2. * np.pi)
 
     def max_facets_on_radius(self):
-        return (self.radius[1] - self.radius[0]) % self.d_facet
+        return (self.radius[1] - self.radius[0]) // self.d_facet
 
-    def select_radii(self):
+    def distribute_facets_on_radius(self):
         '''Distributes facets as evenly as possible along a radius'''
         n = self.max_facets_on_radius()
         d_between = (self.radius[1] - self.radius[0] - n * self.d_facet) / (n + 1)
-        return d_between + 0.5 * self.d_facet + np.arange(n) * (d_between + self.d_facet)
+        return self.radius[0] + d_between + 0.5 * self.d_facet + np.arange(n) * (d_between + self.d_facet)
 
     def place_facets(self):
         '''
@@ -114,7 +120,7 @@ class GratingArrayStructure(object):
             yz-plane to the required facet position on the Rowland torus.
         '''
         pos4d = []
-        radii = self.select_radii()
+        radii = self.distribute_facets_on_radius()
         for r in radii:
             angles = self.distribute_facets_on_arc(r)
             for a in angles:
@@ -122,14 +128,15 @@ class GratingArrayStructure(object):
                 z = r * np.cos(a)
                 def f(x):
                     return self.rowland.quartic(x, y, z)
-                x = optimize.brentq(f, self.z_min, self.z_max)
-                # TBD: Error checking
+                x, brent_out = optimize.brentq(f, self.x_range[0], self.x_range[1], full_output=True)
+                if not brent_out.converged:
+                    raise Exception('Intersection with torus not found.')
                 facet_pos = np.array([x, y, z])
                 facet_normal = np.array(self.rowland.normal(x, y, z))
                 # Find the rotation between [1, 0, 0] and the new normal
                 rot_ax = np.cross([1., 0., 0.], facet_normal)
-                if np.all(np.allclose(rot_ax), 0):
-                    rot_mat = np.eye(4)  #vectors are parallel
+                if np.allclose(rot_ax, 0):
+                    rot_mat = np.eye(4)  # vectors are parallel
                 else:
                     rot_ang = np.arcsin(norm(rot_ax) / norm(facet_normal))
                     rot_mat = transforms3d.axangles.axangle2aff(rot_ax, rot_ang)
--- marxs/design/test/test_design.py
+++ marxs/design/test/test_design.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from ..rowland import RowlandTorus
+from ..rowland import RowlandTorus, GratingArrayStructure
 
 def parametrictorus(R, r, theta, phi):
     x = (R + r * np.cos(theta)) * np.cos(phi)
@@ -27,6 +27,11 @@ def test_torus():
     assert np.allclose(mytorus.quartic(x, y, z), 0.)
 
 def test_torus_normal():
+    '''Compare the analytic normal with a numeric result.
+
+    The test computes a vector along the torus for small d_theta
+    and d_phi. Both of those should be perpendicular to the normal.
+    '''
     R = 2.
     r = 1.
 
@@ -48,3 +53,28 @@ def test_torus_normal():
 
     assert np.allclose(np.einsum('ij,ij->j', vec_normal, vec_delta_theta), 0.)
     assert np.allclose(np.einsum('ij,ij->j', vec_normal, vec_delta_phi), 0.)
+
+def test_GratingArrayStructure():
+    '''Check a GAS for consistency
+
+    The expected numbers for this one are hand-checked and plotted and thus
+    can be taken as known-good to prevent regressions later.
+    '''
+    myrowland = RowlandTorus(10000., 10000.)
+    gas = GratingArrayStructure(myrowland, 30., [10000., 20000.], [300., 600.], phi = [-0.2*np.pi, 0.2*np.pi])
+    assert gas.max_facets_on_arc(300.) == 12
+    angles = gas.distribute_facets_on_arc(315.) % (2. * np.pi)
+    # This is a wrap-around case. Hard to test in general, but here I know the numbers
+    assert np.alltrue((angles < 0.2 * np.pi) | (angles > 1.8 * np.pi))
+    assert gas.max_facets_on_radius() == 10
+    assert np.all(gas.distribute_facets_on_radius() == np.arange(315., 600., 30.))
+    assert len(gas.facets) == 177
+    # fig, axes = plt.subplots(2,2)
+    # for elem in [[axes[0, 0], 0, 1], [axes[0, 1], 0, 2], [axes[1, 0], 1, 2]]:
+    #     for f in gas.facets:
+    #         elem[0].plot(f[elem[1],3], f[elem[2],3], 's')
+    # plt.show()  # or move mouse in window or something.
+
+    # There is a test missing here that the rotational part works correctly.
+    # I just cannot think of a good way to check that right now.
+
