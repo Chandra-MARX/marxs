570c9ec5b34590f721683346a9141606b7fbb242
hamogu <moritz.guenther@gmx.de>
2015-06-13 20:03:59 -0400

    Add utils for zoom and transformation matrices

---
+++ marxs/math/test/test_utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from ..import utils
+from transforms3d.affines import compose
+
+def test_random_mat():
+    '''Compare multiplication of trans and zoom matrixes with transfomrs3d.
+
+    This test ensures that the same conventions are used for ordering.
+    '''
+    rot = np.eye(4)
+    trans = np.random.rand(3)
+    zoom = np.random.rand(3)
+    assert np.allclose(np.dot(utils.translation_mat(trans), np.dot(rot, utils.zoom_mat(zoom))),
+                       compose(trans, rot[:3, :3], zoom))
+
+def test_random_mat_scalar_zoom():
+    '''Compare multiplication of trans and zoom matrixes with transfomrs3d.
+
+    This test ensures that the same conventions are used for ordering.
+    '''
+    rot = np.eye(4)
+    trans = np.random.rand(3)
+    zoom = np.random.rand()
+    zoommat = np.ones(3) * zoom
+    assert np.allclose(np.dot(utils.translation_mat(trans), np.dot(rot, utils.zoom_mat(zoom))),
+                       compose(trans, rot[:3, :3], zoommat))
---
+++ marxs/math/utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def translation2aff(vec):
+    '''Transform 3-d translation vector to affine 4*4 matrix.
+
+    Parameters
+    ----------
+    vec : (3, ) array
+        x, y, z translation vector
+
+    Returns
+    -------
+    m : (4, 4) array
+        affine transformation matrix
+    '''
+    if len(vec) != 3:
+        raise ValueError('3d translation vector expected.')
+    m = np.eye(4)
+    m[:3, 3] = vec
+    return m
+
+def zoom2aff(zoom):
+    '''Transform zoom to affine 4*4 matrix.
+
+    Parameters
+    ----------
+    vec : float or (3, ) array
+        zoom factor for x, y, z dimension (the same zoom is applied to
+        every dimension is the input is a scalar).
+
+    Returns
+    -------
+    m : (4, 4) array
+        affine transformation matrix
+    '''
+    z = np.eye(4)
+    if np.isscalar(zoom):
+        z[:3,:3] = zoom * z[:3, :3]
+    elif len(zoom) == 3:
+        z[:3, :3] = np.diag(zoom)
+    else:
+        raise ValueError('Zoom must be either scalar or 3 element vector.')
+    return z
