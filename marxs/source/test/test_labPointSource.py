d75d01fe8aba5c05c24cad9074a35cdd51c48126
Jason Frost <Jason@Jasons-MacBook-Pro-8.local>
2015-07-01 15:17:21 -0400

    Fixed test files to reflect last change

--- marxs/source/test/test_labPointSource.py
+++ marxs/source/test/test_labPointSource.py
@@ -8,7 +8,7 @@ def test_photon_generation():
 	pos = [1., 1., 1.] 
 	polar = [0., 0., 0.] 
 	rate = 10
-	source = LabSource(pos, polar, rate, 5., 1.) 
+	source = LabSource(pos, polar, rate, 5.)
 				    
 	photons = source.generate_photons(1.)
 	assert np.all(photons['pos'] == np.ones([10, 4]))
