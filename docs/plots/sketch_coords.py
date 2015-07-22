8e26e9436392ef5fe43934c7e16d76d39f65811a
hamogu <moritz.guenther@gmx.de>
2015-07-21 21:56:31 -0400

    add transfomrs3d to readthedocs requirement

--- docs/plots/sketch_coords.py
+++ docs/plots/sketch_coords.py
@@ -7,7 +7,7 @@ import numpy as np
 def optical_element_coords():
    fig = plt.figure()
    ax = fig.add_subplot(111, projection='3d')
-   ax.quiver([2,0,0],[0,2,0],[0,0, 2],[1,0,0],[0,1,0], [0,0,1], length=4, arrow_length_ratio=.05, color='k', lw=2)
+   ax.quiver([2,0,0],[0,2,0],[0,0, 2],[1,0,0],[0,1,0], [0,0,1], length=4, color='k', lw=2)
    ax.bar3d(-1, -1, -1, 1, 2, 2, zsort='average', alpha=0.5, color=['b'] *5 + ['r'], zorder=1)
    ax.set_axis_off()
    ax.text(2.1, 0, 0, 'x', None, fontsize='x-large')
