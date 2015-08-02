04b56c6a77fe1d6b4c2bf52ae32909db64ae99a2
hamogu <moritz.guenther@gmx.de>
2015-08-02 19:23:51 -0400

    mock out c extension for docs build

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,19 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
+
+# Mock oout the C extention, so that readthedocs can build the docs.
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['marxs.optics.marx']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
