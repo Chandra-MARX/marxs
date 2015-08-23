ae01908ea583c2a8436974853c2bc5fe1087e25a
hamogu <moritz.guenther@gmx.de>
2015-08-22 20:16:45 -0400

    simpler way to define transformatino matrix

--- marxs/missions/chandra_hess.py
+++ marxs/missions/chandra_hess.py
@@ -9,6 +9,9 @@ from ..optics import FlatGrating, uniform_efficiency_factory
 from ..simulator import Parallel
 
 class HESS(Parallel):
+
+    id_col = 'facet'
+
     def __init__(self, **kwargs):
         path = os.path.dirname(__file__)
         self.hess = astropy.table.Table.read(os.path.join(path, 'HESSdesign.rdb'))
@@ -24,7 +27,7 @@ class HESS(Parallel):
         kwargs['elem_class'] = FlatGrating
         # Gratings are defined in order MEG, then HEG
         d = [4001.95 * 1e-7] * 192 + [2000.81 * 1e-7] * 144
-        kwargs['elem_args'] = {'order_selector': uniform_efficiency_factory,
+        kwargs['elem_args'] = {'order_selector': uniform_efficiency_factory(),
                                'd': d, 'name' : list(self.hess['hessloc'])}
         super(HESS, self).__init__(**kwargs)
 
@@ -36,6 +39,7 @@ class HESS(Parallel):
         '''
         # take x,y,z components of various vectors and put in numpy arrays
         # Note: This makes (3, N) arrays as opposed to (N, 3) arrays used in other parts of MARXS.
+        # Note: Sometimes an angle has a minus. Beware of active / passive rotations.
         hess = self.hess
 
         cen = np.vstack([hess[s+'c'].data for s in 'xyz'])
@@ -44,28 +48,27 @@ class HESS(Parallel):
         # All those vectors are already normalized
         facnorm = np.vstack([hess[s+'u'].data for s in 'xyz'])
         uxf = np.vstack([hess[s+'uxf'].data for s in 'xyz'])
-        ul = np.vstack([hess[s+'ul'].data for s in 'xyz'])
+        uyf = np.vstack([hess[s+'uyf'].data for s in 'xyz'])
 
-        # Angle between y edge of grating and grove (edges not parallel to grooves)
-        groove_angle = -np.arccos(np.einsum('ij,ij->j',ul, uxf))
-        self.elem_args['groove_angle'] = list(groove_angle)
         zoom = np.array([1.035*25.4/2, 0.920*25.4/2, 1])
 
         pos4ds = []
         for i in range(facnorm.shape[1]):
-            # Find angle and axis to rotate from old normal to facnorm
-            axis1 = np.cross(np.array([1., 0., 0.]), facnorm[:, i])
-            ang1 = np.arccos(facnorm[0, i])  # equivalent to: [1,0,0] * facnorm[:,1]
-            rot1 = axangle2mat(axis1, ang1)
-            # rotate round facnorm until y edges match
-            # 1. Get rotated y axis
-            yrot = np.dot(rot1, np.array([0., 1., 0.]))
-            # 2. yrot and uxf should now both be in the plane of the facet.
-            ang2 = np.arccos(np.dot(yrot, uxf[:, i]))
-            # 3. Get rotation matrix around facet normal.
-            rot2 = axangle2mat(facnorm[:, i], ang2)
-            # Combine all the ingredients into 4d matrix
-            rot = np.dot(rot2, rot1)
+            rot = np.eye(3)
+            rot[:, 0] = facnorm[:, i]
+            rot[:, 1] = uxf[:, i]
+            rot[:, 2] = uyf[:, i]
             pos4d = compose(cen[:, i], rot, zoom)
             pos4ds.append(pos4d)
         return pos4ds
+
+    def generate_elements(self):
+        '''generate elements as usual, set groove direction from table afterwards.'''
+        super(HESS, self).generate_elements()
+        ul = np.vstack([self.hess[s+'ul'].data for s in 'xyz'])
+        ud = np.vstack([self.hess[s+'ud'].data for s in 'xyz'])
+        for i in range(len(self.elements)):
+            # No need to calculate those from groove angle
+            # They are already in the input table.
+            self.elements[i].geometry['e_groove'][:3] = ul[:, i]
+            self.elements[i].geometry['e_perp_groove'][:3] = ud[:, i]
