60f05a7109c97df73b7c82a2c18cee9347be1ed5
hamogu <hgunther@mit.edu>
2016-05-18 14:22:09 -0400

    pass blaze to order_selector

--- marxs/optics/grating.py
+++ marxs/optics/grating.py
@@ -168,13 +168,15 @@ class FlatGrating(FlatOpticalElement):
         d = h2e(self.geometry['e_perp_groove'])
 
         wave = energy2wave / photons['energy'].data[intersect]
-        m, prob = self.order_selector(photons['energy'].data[intersect], photons['polarization'].data[intersect])
         # calculate angle between normal and (ray projected in plane perpendicular to groove)
         # -> this is the blaze angle
         p_perp_to_grooves = norm_vector(p - np.dot(p, l)[:, np.newaxis] * l)
         # Use abs here so that blaze angle is always in 0..pi/2
         # independent of the relative orientation of p and n.
         blazeangle = np.arccos(np.abs(np.dot(p_perp_to_grooves, n)))
+        m, prob = self.order_selector(photons['energy'].data[intersect],
+                                      photons['polarization'].data[intersect],
+                                      blazeangle)
 
         # The idea to calculate the components in the (d,l,n) system separately
         # is taken from MARX
