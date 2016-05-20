1a384e74c5f2c0aea15a6a608160e126d50be315
Brigitta Sipocz <bsipocz@gmail.com>
2016-05-20 02:16:50 +0100

    Importing configparser from six.moves to make it both py2 and py3 compatible

--- marxs/optics/marx_build.py
+++ marxs/optics/marx_build.py
@@ -1,5 +1,5 @@
 from os import path
-from ConfigParser import ConfigParser
+from six.moves.configparser import ConfigParser
 from cffi import FFI
 
 with open ("marxs/optics/cdef.txt", "r") as myfile:
