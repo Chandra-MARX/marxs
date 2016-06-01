5cc4544f9f6a08b6516d399c26e2ebf485b6e61a
hamogu <hgunther@mit.edu>
2016-06-01 12:38:57 -0400

    add LinearCCDArray to imports in design

--- marxs/design/__init__.py
+++ marxs/design/__init__.py
@@ -1,1 +1,3 @@
-from .rowland import RowlandTorus, GratingArrayStructure, find_radius_of_photon_shell
+from .rowland import (RowlandTorus,
+                      GratingArrayStructure, LinearCCDArray,
+                      find_radius_of_photon_shell)
