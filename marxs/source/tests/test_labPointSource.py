d1828d7d4878a9de730afb1ac62f0ae4d496f4ce
Adam Theriault-Shay <adamtheriault-shay@dhcp-18-189-16-113.dyn.MIT.EDU>
2016-05-31 23:51:38 -0400

    added a test that asserts the final component of direction vectors is 0

--- marxs/source/tests/test_labPointSource.py
+++ marxs/source/tests/test_labPointSource.py
@@ -1,5 +1,6 @@
 import numpy as np
 from ..labSource import LabPointSource as LabSource
+from ..labSource import LabPointSourceCone
 
 def test_photon_generation():
 	'''This tests the lab point source. It checks that the starting points are all
@@ -21,3 +22,13 @@ def test_photon_direction():
 
 	photons = source.generate_photons(1.)
 	assert np.all(photons['dir'][:, 1] <= 0)
+
+def test_photon_direction_cone():
+	"This tests the LabPointSourceCone. Checks the optional 'direction' parameter. It also checks that the fourth element of every direction vector is zero"
+	pos = [1., 1., 1.]
+	rate = 10
+	direction = [1.,1.,1.]
+	source = LabPointSourceCone(pos, delta = 1, flux=rate, energy=5., direction = direction)
+
+	photons = source.generate_photons(1.)
+	assert np.all(photons['dir'][:, 3] <= 0)
