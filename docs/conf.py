ad5dc0c82bf3c05a6bd84d609fb1bac32fd2b80e
hamogu <moritz.guenther@gmx.de>
2017-04-02 22:29:33 -0400

    include x3d css and js in marxs repro

--- docs/conf.py
+++ docs/conf.py
@@ -131,7 +131,7 @@ rst_epilog += '''
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = [os.path.abspath(os.path.join(os.path.dirname(__file__), '_static'))]
+html_static_path = ['_static']
 
 
 # Output file base name for HTML help builder.
