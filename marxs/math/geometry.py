031c98f92783bb90f4420f7368582892346428ed
hamogu <moritz.guenther@gmx.de>
2018-05-14 22:41:25 -0400

    New geometry module for forward only interactions

---
+++ marxs/math/geometry.py
@@ -0,0 +1,356 @@
+# Licensed under GPL version 3 - see LICENSE.rst
+from functools import wraps
+from copy import copy
+
+import numpy as np
+from astropy.table import Table, Row
+from transforms3d.affines import decompose44
+
+from ..math.utils import e2h, h2e
+from ..base import SimulationSequenceElement, _parse_position_keywords
+
+
+class Geometry(object):
+    _geometry = {}
+    _display = {}
+
+    def __init__(self, kwargs={}):
+        self.pos4d = _parse_position_keywords(kwargs)
+        #copy class attribute to instance attribute
+        self._geometry = copy(self._geometry)
+
+
+    def __getitem__(self, key):
+        '''This function wraps access to the pos4d matrix.
+
+        This is mostly a convenience method that gives access to vectors from the
+        ``pos4d`` matrix in familiar terms with string labels:
+
+        - ``center``: The ``center`` is the origin of the local coordiante system
+          of the optical elemement. Typically, if will be the center of the
+          active plane, e.g. the center of the mirror surface.
+        - :math:`\hat v_{y,z}`: The box stretches in the y and z direction for
+          :math:`\pm \hat v_y` and :math:`\pm \hat v_z`. In optics, the relevant
+          interaction often happens on a surface, e.g. the surface of a mirror or
+          of a reflection grating. In the defaul configuration, this surface is in
+          the yz-plane.
+        - :math:`\hat v_x`: The thickness of the box is not as important in many
+          cases,
+          but is useful to e.g. render the geometry or to test if elements collide.
+          The box reaches from the yz-plane down to :math:`- \hat v_x`. Note, that
+          this definition means the size parallel to the y and z axis is
+          :math:`2 |\hat v_{y,z}|`, but only :math:`1 |\hat v_{x}|`.
+        - :math:`\hat e_{x,y,z}`: When an object is initialized, it automatically
+          adds unit vectors in the
+          direction of :math:`\hat v_{x,y,z}` called :math:`\hat e_{x,y,z}`.
+        - ``plane``: It also adds the homogeneous coordinates of the active plane
+          as ``plane``.
+        - Other labels get looked up in ``self._geometry`` and if the resulting
+          value is a 4-d vector, it gets transformed with ``pos4d``.
+
+        Not all these labels make sense for every optical element (e.g. a curved
+        mirror does not really have a "plane").
+        Access through this method is slower than direct indexing of ``self.pos4d``.
+        '''
+
+        if key == 'center':
+            return self.pos4d[:, 3]
+        elif key in ['v_x', 'e_x']:
+            val = self.pos4d[:, 0]
+        elif key in ['v_y', 'e_y']:
+            val = self.pos4d[:, 1]
+        elif key in ['v_z', 'e_z']:
+            val = self.pos4d[:, 2]
+        elif key == 'plane':
+            return point_dir2plane(self['center'], self['e_x'])
+        else:
+            val = self._geometry[key]
+            if isinstance(val, np.ndarray) and (val.shape[-1] == 4):
+                val = np.dot(self.pos4d, val)
+        if key[:2] == 'e_':
+            return val / np.linalg.norm(val)
+        else:
+            return val
+
+    def intersect(self, dir, pos):
+        '''Calculate the intersection point between a ray and the element
+
+        Parameters
+        ----------
+        dir : `numpy.ndarray` of shape (N, 4)
+            homogeneous coordinates of the direction of the ray
+        pos : `numpy.ndarray` of shape (N, 4)
+            homogeneous coordinates of a point on the ray
+
+        Returns
+        -------
+        intersect :  boolean array of length N
+            ``True`` if an intersection point is found.
+        interpos : `numpy.ndarray` of shape (N, 4)
+            homogeneous coordinates of the intersection point. Values are set
+            to ``np.nan`` if no intersection point is found.
+        interpos_local : `numpy.ndarray` of shape (N, 2)
+            y and z coordinates in the coordiante system of the active plane.
+        '''
+        raise NotImplementedError
+
+    def display(self, key):
+        if key in self._display:
+            return self._display[key]
+        else:
+            return super(Geometry, self).display(key)
+
+
+class FinitePlane(Geometry):
+    '''Base class for geometrically flat optical elements.
+
+    '''
+
+    _display = {'shape': 'box'}
+
+    loc_coos_name = ['y', 'z']
+    '''name for output columns that contain the interaction point in local coordinates.'''
+
+    def __init__(self, **kwargs):
+        super(FinitePlane, self).__init__(kwargs)
+
+
+    def intersect(self, dir, pos):
+        '''Calculate the intersection point between a ray and the element
+
+        Parameters
+        ----------
+        dir : `numpy.ndarray` of shape (N, 4)
+            homogeneous coordinates of the direction of the ray
+        pos : `numpy.ndarray` of shape (N, 4)
+            homogeneous coordinates of a point on the ray
+
+        Returns
+        -------
+        intersect :  boolean array of length N
+            ``True`` if an intersection point is found.
+        interpos : `numpy.ndarray` of shape (N, 4)
+            homogeneous coordinates of the intersection point. Values are set
+            to ``np.nan`` if no intersection point is found.
+        interpos_local : `numpy.ndarray` of shape (N, 2)
+            y and z coordinates in the coordiante system of the active plane
+            (not normalized to the dimensions of the element in question, but
+            in absolute units).
+        '''
+        k_nominator = np.dot(self['center'] - pos, self['e_x'])
+        k_denom = np.dot(dir, self['e_x'])
+
+        is_parallel = k_denom == 0
+        # To avoid warning for parallel rays, which is handeled expicitly below
+        with np.errstate(divide='ignore'):
+            k = k_nominator / k_denom
+
+        forward = k >= 0  # with k < 0, rays would have to move backwards.
+
+        interpos = pos + k[:, None] * dir
+        vec_center_inter = interpos - self['center']
+        interpos_local = np.vstack([np.dot(vec_center_inter, self['e_y']),
+                                    np.dot(vec_center_inter, self['e_z'])]).T
+
+        intersect = (~is_parallel & forward &
+                     (np.abs(interpos_local[:, 0]) <= np.linalg.norm(self['v_y'])) &
+                     (np.abs(interpos_local[:, 0]) <= np.linalg.norm(self['v_z'])))
+        for i in [interpos, interpos_local]:
+            if dir.ndim == 2:
+                i[~intersect, :3] = np.nan
+            # input of single photon.
+            elif (dir.ndim == 1) and not intersect:
+                i[:3] = np.nan
+
+        return intersect, interpos, interpos_local
+
+
+class Cylinder(Geometry):
+    '''A detector shaped like a ring or tube.
+
+    This detector is shaped like a tube. The form is a circle in the xy plane
+    and and flat along the z-direction.  While most CCDs are flat in practice,
+    the `CircularDetector` simulates a setup that can follow the Rowland circle
+    geometry exactly which is useful, e.g. to study the resolution of a
+    spectrograph without worrying about the details of the detector geometry.
+
+    Parameters
+    ----------
+    position, orientation, zoom, pos4d : see description of `pos4d`
+        The radius of the tube is given by the ``zoom`` keyword, see `pos4d`.
+        Use ``zoom[0] == zoom[1]`` to make a circular tube. ``zoom[0] != zoom[1]`` gives
+        an elliptical profile. ``zoom[2]`` sets the extension in the z direction.
+    pixsize : float
+        size of pixels in mm
+    '''
+    loc_coos_name = ['phi', 'y']
+
+
+    _display = {'shape': 'surface',
+               'coo1': np.linspace(-np.pi, np.pi, 50),
+               'coo2': [-1, 1]}
+
+    @property
+    def phi_lim(self):
+        return self._phi_lim
+
+    @phi_lim.setter
+    def phi_lim(self, value):
+        if (len(value) != 2) or (value[0] > value[1]):
+            raise ValueError('phi_lim has the format [lower limit, upper limit]')
+        for v in value:
+            if (v < -np.pi) or (v > np.pi):
+                raise ValueError('phi_lim must be in range -pi to +pi.')
+        self._phi_lim = value
+        self._display['coo1'] = np.linspace(value[0], value[1], 50)
+
+    def __init__(self, **kwargs):
+        super(Cylinder, self).__init__(kwargs)
+        self.phi_lim = kwargs.pop('phi_lim', [-np.pi, np.pi])
+
+
+    def __getitem__(self, value):
+        if value == 'R':
+            trans, rot, zoom, shear = decompose44(self.pos4d)
+            return zoom[0]
+        else:
+            return super(Cylinder, self).__getitem__(value)
+
+    @classmethod
+    def from_rowland(cls, rowland, width):
+        '''Generate a `CircularDetector` from a `RowlandTorus`.
+
+        Parameters
+        ----------
+        rowland : `~marxs.design.RowlandTorus`
+            The circular detector is constructed to fit exactly into the
+            Rowland Circle defined by ``rowland``.
+        width : float
+            Half-width of the tube in the flat direction (z-axis) in mm
+        '''
+        # Step 1: Get position and size from Rowland torus
+        pos4d_circ = compose([rowland.R, 0, 0], np.eye(3), [rowland.r, rowland.r, width])
+        # Step 2: Transform to global coordinate system
+        pos4d_circ = np.dot(rowland.pos4d, pos4d_circ)
+        # Step 3: Make detector
+        return cls(pos4d=pos4d_circ, phi_offset=-np.pi)
+
+
+    def intersect(self, dir, pos, transform=True):
+        '''Calculate the intersection point between a ray and the element
+
+        Parameters
+        ----------
+        dir : `numpy.ndarray` of shape (N, 4)
+            homogeneous coordinates of the direction of the ray
+        pos : `numpy.ndarray` of shape (N, 4)
+            homogeneous coordinates of a point on the ray
+        transform : bool
+            If ``True``, input is in global coordinates and needs to be transformed
+            here for the calculations; if ``False`` input is in local coordinates.
+
+        Returns
+        -------
+        intersect :  boolean array of length N
+            ``True`` if an intersection point is found.
+        interpos : `numpy.ndarray` of shape (N, 4)
+            homogeneous coordinates of the intersection point. Values are set
+            to ``np.nan`` is no intersecton point is found.
+        interpos_local : `numpy.ndarray` of shape (N, 2)
+            phi, z coordiantes (in the local frame) for one of the intersection points.
+            If both intersection points are required, reset ``self.inner`` and call this
+            function again.
+        '''
+        # This could be moved to a general function
+        if not np.all(dir[:, 3] == 0):
+            raise ValueError('First input must be direction vectors.')
+        # Could test pos, too...
+        if transform:
+            invpos4d = np.linalg.inv(self.pos4d)
+            dir = np.dot(invpos4d, dir.T).T
+            pos = np.dot(invpos4d, pos.T).T
+
+        xyz = h2e(pos)
+        dir_e = h2e(dir)
+
+        # Solve quadratic equation in steps. a12 = (-xr +- sqrt(xr - r**2(x**2 - R**2)))
+        xy = xyz[:, :2]
+        r = dir[:, :2]
+        underroot = (np.einsum('ij,ij->i', xy, r))**2 - np.sum(r**2, axis=1) * (np.sum(xy**2, axis=1) - 1.)
+        # List of intersect in xy plane.
+        intersect = (underroot >= 0)
+        i = intersect  # just a shorthand because it's used so much below
+
+        interpos_local = np.ones((pos.shape[0], 2))
+        interpos_local[:] = np.nan
+        interpos = np.ones_like(pos)
+        interpos[:] = np.nan
+
+        if intersect.sum() > 0:
+            i_ind = intersect.nonzero()[0]
+            b = np.sum(xy[i] * r[i], axis=1)
+            denom = np.sum(r[i]**2, axis=1)
+            a1 = (- b + np.sqrt(underroot[i])) / denom
+            a2 = (- b - np.sqrt(underroot[i])) / denom
+            xy_1 = xy[i, :] + a1[:, np.newaxis] * r[i, :]
+            phi_1 = np.arctan2(xy_1[:, 1], xy_1[:, 0])
+            xy_2 = xy[i, :] + a2[:, np.newaxis] * r[i, :]
+            phi_2 = np.arctan2(xy_2[:, 1], xy_2[:, 0])
+            # 1, 2 look like hits in x,y but might still miss in z
+            z_1 = xyz[i, 2] + a1 * dir[i, 2]
+            z_2 = xyz[i, 2] + a2 * dir[i, 2]
+            hit_1 = ((a1 >= 0) & (np.abs(z_1) <= 1.) &
+                     (phi_1 >= self.phi_lim[0]) & (phi_1 <= self.phi_lim[1]))
+            # Use hit_2 only if a2 is closer than hit_1
+            hit_2 = ((a2 >= 0) & (a2 <= a1) & (np.abs(z_2) <= 1.) &
+                     (phi_2 >= self.phi_lim[0]) & (phi_2 <= self.phi_lim[1]))
+            intersect[i_ind] = hit_1 | hit_2
+            # Set values into array from either point 1 or 2
+            interpos_local[i_ind[hit_1], 0] = phi_1[hit_1]
+            interpos_local[i_ind[hit_1], 1] = z_1[hit_1]
+
+            interpos_local[i_ind[hit_2], 0] = phi_2[hit_2]
+            interpos_local[i_ind[hit_2], 1] = z_2[hit_2]
+            # Calculate pos for point 1 or 2 in local xyz coord system
+            interpos[i_ind[hit_1], :] = e2h(xyz[i_ind, :] + a1[:, None] * dir_e[i_ind, :], 1)[hit_1, :]
+            interpos[i_ind[hit_2], :] = e2h(xyz[i_ind, :] + a2[:, None] * dir_e[i_ind, :], 1)[hit_2, :]
+
+
+            trans, rot, zoom, shear = decompose44(self.pos4d)
+            # interpos_local in z direction is in local coordinates, i.e.
+            # the x coordiante is 0..1, but we want that in units of the
+            # global coordinate system.
+            interpos_local[:, 1] = interpos_local[:, 1] * zoom[2]
+            interpos = np.dot(self.pos4d, interpos.T).T
+
+        return intersect, interpos, interpos_local
+
+
+    def parametric_surface(self, phi, z=np.array([-1, 1])):
+        '''Parametric description of the tube.
+
+        This is just another way to obtain the shape of the tube, e.g.
+        for visualization.
+
+        Parameters
+        ----------
+        phi : np.array
+            ``phi`` is the angle around the tube profile.
+        z : np.array
+            The coordiantes along the radius coordinate.
+
+        Returns
+        -------
+        xyzw : np.array
+            Ring coordinates in global homogeneous coordinate system.
+        '''
+        phi = np.asanyarray(phi)
+        z = np.asanyarray(z)
+        if (phi.ndim != 1) or (z.ndim != 1):
+            raise ValueError('input parameters have 1-dim shape.')
+        phi, z = np.meshgrid(phi, z)
+        x = np.cos(phi)
+        y = np.sin(phi)
+        w = np.ones_like(z)
+        coos = np.array([x, y, z, w]).T
+        return np.einsum('...ij,...j', self.pos4d, coos)
---
+++ marxs/math/tests/test_geometry.py
@@ -0,0 +1,227 @@
+# Licensed under GPL version 3 - see LICENSE.rst
+import numpy as np
+from astropy.table import Table
+import transforms3d
+import pytest
+
+from ..geometry import Cylinder, FinitePlane
+from ...tests import closeornan
+from ...math.utils import h2e
+from ...design import RowlandTorus
+
+
+def test_intersect_plane_forwards():
+
+    pos = np.array([[20., 0., 0.8, 1.],  # start above
+                    [0., .0, 0.4, 1.],  # start inside
+                    [-5., .1, -.2, 1]]) # start below
+    dir = np.array([[-.1, 0., 0., 0],
+                    [-1., 0, 0.02, 0.],
+                    [-2., -0.02, 0.023, 0.]])
+
+    plane = FinitePlane()
+    intersect, interpos, inter_local = plane.intersect(dir, pos)
+    assert np.all(intersect == [True, True, False])
+    intersect, interpos, inter_local = plane.intersect(-dir, pos)
+    assert np.all(intersect == [False, True, True])
+
+def test_intersect_plane_zoomed():
+    pos = np.array([[1., 4., 0.8, 1.],
+                    [1., -4., 8., 1.],
+                    [1., -6., -8., 1]])
+    dir = np.array([[-.1, 0., 0., 0],
+                    [-1., 0, 0.02, 0.],
+                    [-2., -0.02, 0.023, 0.]])
+
+    plane = FinitePlane(zoom=[1, 5, 10])
+    intersect, interpos, inter_local = plane.intersect(dir, pos)
+    assert np.all(intersect == [True, True, False])
+    assert np.all(interpos[0, :] == [0, 4, .8, 1])
+    assert np.all(inter_local[0, ] == [4, .8])
+
+def test_intersect_plane_moved():
+
+    pos = np.array([[4.1, 5.5, 6.8, 1.],
+                    [5., 4.1, 4.99, 1.],
+                    [4.1, 3., 6., 1]])
+    dir = np.array([[-.1, 0., 0., 0],
+                    [-1., 0, 0.02, 0.],
+                    [-2., -0.02, 0.023, 0.]])
+
+    plane = FinitePlane(position=[4,5,6])
+    intersect, interpos, inter_local = plane.intersect(dir, pos)
+    assert np.all(intersect == [True, True, False])
+    assert np.all(interpos[1, :] == [4, 4.1, 5.01, 1])
+    assert np.allclose(inter_local[1, :], [-0.9, -.99])
+
+
+def test_intersect_tube_miss():
+    '''Rays passing at larger radius or through the center miss the tube.'''
+    circ = Cylinder()
+    # Passing at larger radius
+    intersect, interpos, inter_local = circ.intersect(np.array([[1., 0., .1, 0],
+                                                                [-1., -1., 0., 0.]]),
+                                                         np.array([[0, -1.1, 0., 1.],
+                                                                   [2., 0., 0., 1.]]))
+    assert np.all(intersect == False)
+    assert np.all(np.isnan(interpos))
+    assert np.all(np.isnan(inter_local))
+
+    # Through the center almost parallel to z-axis
+    intersect, interpos, inter_local = circ.intersect(np.array([[0., 0.1, 1., 0]]),
+                                                         np.array([[0.5, 0.5, 0., 1.]]))
+    assert np.all(intersect == False)
+    assert np.all(np.isnan(interpos))
+    assert np.all(np.isnan(inter_local))
+
+    # Repeat with a tube that's moved to make sure we did not mess up local and
+    # global coordinates in the implementation
+    circ = Cylinder(position=[0.8, 0.8, 1.2])
+    intersect, interpos, inter_local = circ.intersect(np.array([[1., 0., .0, 0],
+                                                                [1., 0., 0., 0.]]),
+                                                         np.array([[0., 0., 0., 1.],
+                                                                   [0., -0.3, 1., 1.]]))
+    assert np.all(intersect == False)
+    assert np.all(np.isnan(interpos))
+    assert np.all(np.isnan(inter_local))
+
+def test_intersect_tube_rays_forward():
+    '''Rays only intersect if propagating them forward works'''
+    pos = np.array([[20., 0., 0.8, 1.],  # start above
+                    [0., .0, 0.4, 1.],  # start inside
+                    [-5., .1, -.2, 1]]) # start below
+    dir = np.array([[-.1, 0., 0., 0],
+                    [-1., 0, 0.02, 0.],
+                    [-2., -0.02, 0.023, 0.]])
+
+    circ = Cylinder(zoom=[1, 1, 1])
+    intersect, interpos, inter_local = circ.intersect(dir, pos)
+    assert np.all(intersect == [True, True, False])
+    intersect, interpos, inter_local = circ.intersect(-dir, pos)
+    assert np.all(intersect == [False, True, True])
+
+
+def test_intersect_tube_hitmiss_zoomed():
+    '''Rays that hit one tube, but miss a shorter one'''
+    dir = np.array([[-.1, 0., 0., 0], [-1., 0, 0.2, 0.]])
+    pos = np.array([[20., 0., 0.8, 1.], [2., .0, 0.4, 1.]])
+
+    circ = Cylinder(zoom=[1, 1, 1])
+    intersect, interpos, inter_local = circ.intersect(dir, pos)
+    assert np.all(intersect == True)
+
+    circ = Cylinder(zoom=[1, 1, .5])
+    intersect, interpos, inter_local = circ.intersect(dir, pos)
+    assert np.all(intersect == False)
+    assert np.all(np.isnan(interpos))
+    assert np.all(np.isnan(inter_local))
+
+def test_intersect_tube_2points():
+    dir = np.array([[-.1, 0., 0., 0], [-0.5, 0, 0., 0.], [-1., 0., 0., 0.]])
+    pos = np.array([[50., 0., 0., 1.], [10., .5, 0., 1.], [2, 0, .3, 1.]])
+
+    circ = Cylinder()
+    intersect, interpos, inter_local = circ.intersect(dir, pos)
+    assert np.all(intersect == True)
+    assert np.allclose(h2e(interpos), np.array([[1., 0., 0.],
+                                                [np.sqrt(0.75), 0.5, 0.],
+                                                [1, 0., .3]]))
+    assert np.allclose(inter_local, np.array([[0., np.arcsin(0.5), 0.],
+                                              [0., 0., 0.3]]).T)
+
+def test_intersect_tube_2points_transformed():
+    '''inter_local should be the same as interpos,
+    even if we make the tube longer in z'''
+    pos = np.array([[50., 0., 0., 1.], [10., .5, 0., 1.], [2, 0, .3, 1.]])
+    dir = np.array([[-.1, 0., 0., 0], [-0.5, 0, 0., 0.], [-1., 0., 0., 0.]])
+
+    exp_inter_loc = np.array([[0., np.arcsin(0.5), 0.],
+                              [0., 0., 0.3]]).T
+    exp_interpos = np.array([[1., 0., 0.],
+                             [np.sqrt(0.75), 0.5, 0.],
+                             [1, 0., .3]])
+    circ = Cylinder(zoom=[1, 1., 5.])
+    intersect, interpos, inter_local = circ.intersect(dir, pos)
+    assert np.all(intersect == True)
+    assert np.allclose(h2e(interpos), exp_interpos)
+    assert np.allclose(inter_local, exp_inter_loc)
+    # Rotate: interpos should be the same as in the unrotated case.
+    # Beware of rotation direction!
+    orient = transforms3d.axangles.axangle2mat([0, 0, 1], -.3)
+    circ = Cylinder(orientation=orient)
+    intersect, interpos, inter_local = circ.intersect(dir, pos)
+    assert np.all(intersect == True)
+    assert np.allclose(h2e(interpos), exp_interpos)
+    assert np.allclose(inter_local, np.array([[0.3, 0.3 + np.arcsin(0.5), 0.3],
+                                              [0., 0., 0.3]]).T)
+
+    # Shift: inter_local is the same, but interpos changes
+    circ = Cylinder(position=[-.3, 0, 0])
+    intersect, interpos, inter_local = circ.intersect(dir, pos)
+    assert np.all(intersect == True)
+    assert np.allclose(h2e(interpos), np.array([[.7, 0, 0],
+                                                [np.sqrt(0.75) -.3, 0.5, 0.],
+                                                [.7, 0., .3]]))
+    assert np.allclose(inter_local, exp_inter_loc)
+
+def test_intersect_tube_2points_outside():
+    '''Setting phi_lim
+
+    These are the same numbers, but with the phi_lim set, all intersection are
+    at the "bottom" of the cylinder.'''
+    pos = np.array([[50., 0., 0., 1.], [10., .5, 0., 1.], [2, 0, .3, 1.]])
+    dir = np.array([[-.1, 0., 0., 0], [-0.5, 0, 0., 0.], [-1., 0., 0., 0.]])
+
+    circ = Cylinder(phi_lim=[.6, np.pi])
+    intersect, interpos, inter_local = circ.intersect(dir, pos)
+    assert np.all(intersect == True)
+    assert np.allclose(h2e(interpos), np.array([[-1., 0., 0.],
+                                                [-np.sqrt(0.75), 0.5, 0.],
+                                                [-1, 0., .3]]))
+    assert np.allclose(inter_local, np.array([[np.pi, np.pi - np.arcsin(0.5), np.pi],
+                                              [0., 0., 0.3]]).T)
+    # phi_lims that guarantee a miss
+    circ = Cylinder(phi_lim=[-.4, -.3])
+    intersect, interpos, inter_local = circ.intersect(dir, pos)
+    assert np.all(intersect == False)
+    assert np.all(np.isnan(interpos))
+    assert np.all(np.isnan(inter_local))
+
+def test_phi_lim_verification():
+    '''Check Errors for wrong phi_lim format.'''
+    with pytest.raises(ValueError) as e:
+        circ = Cylinder(phi_lim=[-.4, -.3, .4])
+    assert '[lower limit, upper limit]' in str(e.value)
+
+    with pytest.raises(ValueError) as e:
+        circ = Cylinder(phi_lim=[-.4, -.5])
+    assert '[lower limit, upper limit]' in str(e.value)
+
+    with pytest.raises(ValueError) as e:
+        circ = Cylinder(phi_lim=[-.4, 5])
+    assert 'range -pi to +pi' in str(e.value)
+
+
+def test_intersect_tube_2points_translation():
+    '''Repeat test above with a tube that's moved and zoomed to make sure we
+    did not mess up local and global coordinates in the implementation
+    '''
+    circ = Cylinder(position=[0.8, 0.8, 1.2], zoom=[1, 1, 2])
+    intersect, interpos, inter_local = circ.intersect(np.array([[1., 0., .0, 0],
+                                                                [1., 0., 0., 0.]]),
+                                                         np.array([[0., 0., 0., 1.],
+                                                                   [0., 0., 1., 1.]]))
+    assert np.all(intersect == True)
+    assert np.allclose(h2e(interpos), np.array([[.2, 0., 0.],
+                                                [.2, 0., 1.]]))
+
+def test_tube_parametric():
+    '''Generate points on surface using parametric. Then make rays for intersection
+    which should return those points as intersection points.'''
+    circ = Cylinder(zoom=[2., 3., 4.])
+    parametric = circ.parametric_surface(phi=[-.1, 0., 1.])
+    parametric = parametric.reshape((6, 4))
+    # select dir so that it's in the right direction to recover these points.
+    dir = np.tile([1, 0, 0, 0], (6, 1))
+    intersect, interpos, inter_local = circ.intersect(dir, parametric)
+    assert np.allclose(parametric, interpos)
