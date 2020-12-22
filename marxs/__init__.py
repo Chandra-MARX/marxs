c75791ae346708dade0d6b17bc03d9f402069189
hamogu <moritz.guenther@gmx.de>
2020-12-22 11:53:15 -0500

    install marx c into gh actions
    install marx c into gh actions
    
    install marx c into gh actions
    
    install marx c into gh actions
    
    trim to 1 action
    
    trim to 1 action
    
    cat setup
    
    cat setup
    
    add echos
    
    add echos
    
    add echos
    
    use pwd
    
    GH Action CI passes

--- marxs/__init__.py
+++ marxs/__init__.py
@@ -8,10 +8,5 @@ This is an Astropy affiliated package.
 from ._astropy_init import *
 # ----------------------------------------------------------------------------
 
-# For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:
-    pass
-
-
 '''Convert from photon energy in keV to wavelength in mm'''
 energy2wave = 1.2398419292004202e-06
