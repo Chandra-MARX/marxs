572afc32a6ed6ccdce893d8a77b1e858ce740e0d
H. Moritz Günther <hgunther@mit.edu>
2026-01-27 21:01:05 -0500

    Update CI workflows

--- marxs/__init__.py
+++ marxs/__init__.py
@@ -2,11 +2,5 @@
 This is an Astropy affiliated package.
 """
 
-# Affiliated packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *
-# ----------------------------------------------------------------------------
-
 '''Convert from photon energy in keV to wavelength in mm'''
 energy2wave = 1.2398419292004202e-06
