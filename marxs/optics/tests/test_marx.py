80e2fc8a946edc4b402a978058fa27783e474d6c
H. Moritz Günther <hgunther@mit.edu>
2026-01-30 12:21:40 -0500

    Do not test MARX C code in CI
    Skip test that require marx C code if not installed
    
    The purpose is to make the test suite pass without the MARX C code, if it's not installed.
    
    In particular, it currently doesn't work on CI and it's very hard to debug why some path path are not passed correctly to gcc.
    There are only very few tests and there is significant overhead in installing the MARX C code in CI, so it might be a good idea to remove that from CI anyway.

--- marxs/optics/tests/test_marx.py
+++ marxs/optics/tests/test_marx.py
@@ -12,11 +12,16 @@ from scipy.stats import ks_2samp
 from astropy.coordinates import SkyCoord
 import astropy.units as u
 from astropy.utils.data import get_pkg_data_filename
+import pytest
 
 import marxs
 import marxs.source
 import marxs.optics.marx
 
+
+@pytest.mark.skipif(
+    "not marxs.optics.marx.HAS_MARX", reason="MARX C code not available"
+)
 def test_noexplicettimedependence():
     '''In an older implementation, the first half of all photons went of
     mirror shell 0, which turned out to be due to ``sorted_index`` being a int,
